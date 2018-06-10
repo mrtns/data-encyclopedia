6576794c7fc3864daf1ce5d9281182af69b5e2cc
Martin Suchanek <mrtn@nrd.io>
2018-06-10 14:35:32 +0200

    Add tests for gatsby-remark-relative-link-adapter

--- plugins/gatsby-remark-relative-link-adapter/__tests__/index.js
+++ plugins/gatsby-remark-relative-link-adapter/__tests__/index.js
@@ -1,3 +1,26 @@
-test("test", () => {
-  expect(true).toBe(true);
+const Remark = require("remark");
+const visit = require(`unist-util-visit`);
+
+const plugin = require("../");
+
+const remark = new Remark().data(`settings`, {
+  commonmark: true,
+  footnotes: true,
+  pedantic: true
+});
+
+describe("plain markdown links", () => {
+  test("it should prefix ../ and remove the .md extension", () => {
+    const markdownAST = remark.parse(
+      `
+      [a-markdown-entry](./a-markdown-entry.md)
+      `.trim()
+    );
+
+    const transformed = plugin({ markdownAST });
+
+    visit(transformed, "link", node => {
+      expect(node.url).toEqual("../a-markdown-entry/");
+    });
+  });
 });
