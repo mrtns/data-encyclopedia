91d9eb8ee4695ad8d036a2fabf0a18f137c7b2f7
Martin Suchanek <mrtn@nrd.io>
2018-06-13 10:25:58 +0200

    List entries

---
+++ src/pages/entries-by-physical-folder.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Link from "gatsby-link";
+
+export default ({ data }) => {
+  const entries = data.allMarkdownRemark.edges.map(edge => (
+    <li>
+      <Link to={edge.node.fields.path}>{edge.node.id}</Link>
+    </li>
+  ));
+  return (
+    <div>
+      <div>
+        <h1>All Entries, by Physical Folder Structure</h1>
+      </div>
+      <ul>{entries}</ul>
+    </div>
+  );
+};
+
+export const query = graphql`
+  query EntriesByPhysicalFolderQuery {
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          fields {
+            path
+          }
+        }
+      }
+    }
+  }
+`;
