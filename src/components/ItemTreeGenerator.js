41191e82d4d55801a03f25dfe2c3c9d777284aaa
Martin Suchanek <mrtn@nrd.io>
2018-06-15 12:01:38 +0200

    ItemTreeGenerator: single path node returns tree with single node

--- src/components/ItemTreeGenerator.js
+++ src/components/ItemTreeGenerator.js
@@ -1,4 +1,21 @@
-function getTreeFromPathNodes() {
-  return [];
+function getChildren(allPathNodes, parentPathNode) {
+  const tree = [];
+
+  for (var pathNode of allPathNodes) {
+    console.log(pathNode);
+    if (pathNode.dir !== parentPathNode) continue;
+
+    tree.push({
+      path: pathNode.path,
+      node: pathNode,
+      children: getChildren(allPathNodes, pathNode.base)
+    });
+  }
+
+  return tree;
+}
+
+function getTreeFromPathNodes(pathNodes) {
+  return getChildren(pathNodes, "/");
 }
 module.exports = { getTreeFromPathNodes };
--- src/components/ItemTreeGenerator.test.js
+++ src/components/ItemTreeGenerator.test.js
@@ -1,8 +1,22 @@
 const itemTreeGenerator = require("./ItemTreeGenerator");
+const itemPathNodeGenerator = require("./ItemPathNodeGenerator");
 
 describe("given an empty item path node list", () => {
   test("it should return an empty list", () => {
     const result = itemTreeGenerator.getTreeFromPathNodes([]);
     expect(result).toEqual([]);
+  });
+});
+
+describe("given a single path node", () => {
+  test("it should return a tree with a single node", () => {
+    const path_node_list_with_single_node = [
+      { path: "/", base: "", dir: "/", root: "/", resolvesToItem: true }
+    ];
+    const result = itemTreeGenerator.getTreeFromPathNodes(
+      path_node_list_with_single_node
+    );
+    expect(result).toHaveLength(1);
+    expect(result).toContainObject({ path: "/", children: [] });
   });
 });
