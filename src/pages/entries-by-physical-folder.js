96d9d5c6bf702f985e2cbbdef214564db500bcc2
Martin Suchanek <mrtn@nrd.io>
2018-06-16 00:06:43 +0200

    Display raw pathified and treeified entries

--- src/pages/entries-by-physical-folder.js
+++ src/pages/entries-by-physical-folder.js
@@ -1,7 +1,27 @@
 import React from "react";
 import Link from "gatsby-link";
+import Pathify from "../components/ItemPathNodeGenerator";
+import Treeify from "../components/ItemTreeGenerator";
 
 export default ({ data }) => {
+  const entryPathNodes = Pathify.getAllPathNodes(
+    data.allMarkdownRemark.edges.map(e => e.node.fields.path)
+  );
+
+  const entryPathNodesUi = entryPathNodes.map(n => (
+    <li>
+      <pre>{JSON.stringify(n)}</pre>
+    </li>
+  ));
+
+  const entryPathsTree = Treeify.getTreeFromPathNodes(entryPathNodes);
+
+  const entryPathsTreeUi = entryPathsTree.map(e => (
+    <li>
+      <pre>{JSON.stringify(e)}</pre>
+    </li>
+  ));
+
   const entries = data.allMarkdownRemark.edges.map(edge => (
     <li>
       <Link to={edge.node.fields.path}>{edge.node.fields.path}</Link>
@@ -9,6 +29,8 @@ export default ({ data }) => {
   ));
   return (
     <div>
+      <ul>{entryPathNodesUi}</ul>
+      <ul>{entryPathsTreeUi}</ul>
       <div>
         <h1>All Entries, by Physical Folder Structure</h1>
       </div>
