ffd630663bf27f06e0760d1f6faa48cad75e495b
Martin Suchanek <mrtn@nrd.io>
2018-06-15 11:21:08 +0200

    Move expect extend to global scope

--- src/components/ItemPathNodeGenerator.test.js
+++ src/components/ItemPathNodeGenerator.test.js
@@ -1,32 +1,5 @@
 const itemPathNodeGenerator = require("./ItemPathNodeGenerator");
 
-expect.extend({
-  toContainObject(received, argument) {
-    const pass = this.equals(
-      received,
-      expect.arrayContaining([expect.objectContaining(argument)])
-    );
-
-    if (pass) {
-      return {
-        message: () =>
-          `expected ${this.utils.printReceived(
-            received
-          )} not to contain object ${this.utils.printExpected(argument)}`,
-        pass: true
-      };
-    } else {
-      return {
-        message: () =>
-          `expected ${this.utils.printReceived(
-            received
-          )} to contain object ${this.utils.printExpected(argument)}`,
-        pass: false
-      };
-    }
-  }
-});
-
 describe("given an empty path list", () => {
   test("it should return an empty object", () => {
     const result = itemPathNodeGenerator.getAllPathNodes([]);
