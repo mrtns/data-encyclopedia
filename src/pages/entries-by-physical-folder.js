f03064aab783e2cb3301d13d3776fe4f112810cc
Martin Suchanek <mrtn@nrd.io>
2018-06-23 12:29:01 +0200

    Remove debug sections

--- src/pages/entries-by-physical-folder.js
+++ src/pages/entries-by-physical-folder.js
@@ -42,38 +42,10 @@ export default ({ data }) => {
     data.allMarkdownRemark.edges.map(e => e.node.fields.path)
   );
 
-  const entryPathNodesUi = entryPathNodes.map(n => (
-    <li>
-      <pre>{JSON.stringify(n)}</pre>
-    </li>
-  ));
-
   const entryPathsTree = Treeify.getTreeFromPathNodes(entryPathNodes);
 
-  const entryPathsTreeUi = entryPathsTree.map(e => (
-    <li>
-      <pre>{JSON.stringify(e)}</pre>
-    </li>
-  ));
-
-  const entries = data.allMarkdownRemark.edges.map(edge => (
-    <li>
-      <Link to={edge.node.fields.path}>{edge.node.fields.path}</Link>
-    </li>
-  ));
   return (
     <div>
-      <div>
-        <h1>Data</h1>
-        <h2>Path node data</h2>
-        <ul>{entryPathNodesUi}</ul>
-        <h2>Tree node data</h2>
-        <ul>{entryPathsTreeUi}</ul>
-      </div>
-      <div>
-        <h1>All Entries</h1>
-      </div>
-      <ul>{entries}</ul>
       <div>
         <h1>All Entries, by Physical Folder Structure</h1>
         <EntryTree nodes={entryPathsTree} />
